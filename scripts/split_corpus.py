2fd48cb4a65bc7fe4ce75a2117d4c0040549b58e
Tiberiu Boros <boros@adobe.com>
2019-10-23 13:49:31 +0300

    Syntax error corrected

--- scripts/split_corpus.py
+++ scripts/split_corpus.py
@@ -1,16 +1,17 @@
 import sys
-if len(sys.argv)<2:
-    print("Usage: split_corpus.py <source> <train> <dev>"
+
+if len(sys.argv) < 2:
+    print("Usage: split_corpus.py <source> <train> <dev>")
     sys.exit(0)
-f_train=open(sys.argv[2], 'w')
-f_dev=open(sys.argv[3],'w')
-cnt=0
+
+f_train = open(sys.argv[2], 'w')
+f_dev = open(sys.argv[3], 'w')
+cnt = 0
 for line in open(sys.argv[1]):
-	cnt+=1
-	if cnt%10==0:
-		f_dev.write(line.strip()+'\n')
-	else:
-		f_train.write(line.strip()+'\n')
+    cnt += 1
+    if cnt % 10 == 0:
+        f_dev.write(line.strip() + '\n')
+    else:
+        f_train.write(line.strip() + '\n')
 f_train.close()
 f_dev.close()
-
